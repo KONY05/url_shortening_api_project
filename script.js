865603e78eb58063ddee82287776ae81b32b0dd2
konyinsola <konyinsolaodesanya@gmail.com>
2024-06-26 09:20:12 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+'use strict';
+
+const userInput = document.querySelector('#user_input');
+const shortenIt_btn = document.querySelector('#shorten_btn');
+const copy_btn = document.querySelector('.copyBtn');
